6657b1d5f34f856c789a816c310f2041002bc223
OmarRMC <mamani.capcha.omarrodrigo@gmail.com>
2025-03-08 20:09:06 -0400

    Se modifico al pusher a reverb

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -10,21 +10,21 @@ window.Pusher = Pusher;
 
 window.Echo = new Echo({
     // Opción para usar Pusher
-    broadcaster: 'pusher',
-    // key: process.env.MIX_PUSHER_APP_KEY,
-    // cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-    key: import.meta.env.VITE_PUSHER_APP_KEY, // Usar import.meta.env
-    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER, // Usar import.meta.env
-    forceTLS: true
+    // broadcaster: 'pusher',
+    // // key: process.env.MIX_PUSHER_APP_KEY,
+    // // cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+    // key: import.meta.env.VITE_PUSHER_APP_KEY, // Usar import.meta.env
+    // cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER, // Usar import.meta.env
+    // forceTLS: true
     
     // Opción para usar Reverb
-    // broadcaster: 'reverb',
-    // key: import.meta.env.VITE_REVERB_APP_KEY,
-    // wsHost: import.meta.env.VITE_REVERB_HOST,
-    // wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
-    // wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-    // forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
-    // enabledTransports: ['ws', 'wss'],
+    broadcaster: 'reverb',
+    key: import.meta.env.VITE_REVERB_APP_KEY,
+    wsHost: import.meta.env.VITE_REVERB_HOST,
+    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
+    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+    enabledTransports: ['ws', 'wss'],
 });
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
